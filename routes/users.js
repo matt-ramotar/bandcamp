ebeba057ade01dcc87b37186c407adf591f5a973
johnpatrickanders <johnpatrickanders@gmail.com>
2020-09-08 16:46:05 -0400

    modifying bin/www

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { csrfProtection, asyncHandler } = require('./utils');
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
