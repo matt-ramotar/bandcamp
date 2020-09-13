699ba5f8ece2af7a2707171d8b34b33f12bf0cc9
Aaron Cygnarowicz <aaroncygnarowicz@Aarons-MacBook-Pro.local>
2020-09-12 22:35:17 -0400

    favorite artist button functioning

--- public/js/home.js
+++ public/js/home.js
@@ -56,9 +56,13 @@ const getFavoriteArtists = async () => {
 const createFavoriteArtist = (artist) => {
     // console.log("artist", artist)
     const artistName = artist.name
+    console.log("name", artistName)
+    let button = document.createElement('button');
+    button.setAttribute('value', artistName);
+    button.innerHTML = `${artistName}`
     // console.log("name", artistName)
     let favoriteArtistDiv = document.querySelector(".favoriteArtists")
-    favoriteArtistDiv.innerHTML = favoriteArtistDiv.innerHTML + `| ${artistName} |`
+    favoriteArtistDiv.appendChild(button);
 
 }
 
@@ -72,3 +76,42 @@ const populateFavoriteArtist = async () => {
 }
 
 populateFavoriteArtist();
+
+const favoriteArtistContainer = document.querySelector('.favoriteArtists');
+favoriteArtistContainer.addEventListener('click', async (e) => {
+    e.preventDefault();
+    let search = await e.target.value;
+    const body = { search };
+    console.log(body)
+    const searchResults = document.querySelector('.search-results');
+    const res = await fetch('/api/songs', {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+    const data = await res.json();
+    if (!res.ok) {
+        const { message } = data;
+        const errorsContainer = document.querySelector('#errors-container');
+        errorsContainer.innerHTML = "Error:" + message + ", please try again.";
+        return;
+    }
+    console.log(data.song);
+    const songs = data.song;
+    let HTMLString = "";
+    for (let i = 0; i < songs.length; i++) {
+        let song = songs[i];
+        let title = song.title;
+        if (!title) continue;
+        HTMLString += `
+      <div class="heartIcon search-results-result" id=${song.id}>
+      <i class="far fa-heart heartIcon"></i>
+      ${title}
+      </div>`
+    }
+    console.log("test")
+    console.log(HTMLString);
+    searchResults.innerHTML = `<div id="search-results">SONG RESULTS...</div> ${HTMLString}`;
+})
